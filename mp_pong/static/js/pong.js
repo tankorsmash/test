78550f47ce7b044aca2c2249643ad4a0acb6b501
TankorSmash <tankorsmash@gmail.com>
2013-03-29 00:06:53 -0400

    Can now score and ball bounces off walls
    Need to make the paddles move and the ball to bounce of paddles. I've got
    maybe 1 hour in me tops, left. This will be tricky.
    
    TODO:
    
    Paddle control                    2hrs
    - Key input system                1hrs
    Paddle ball bounce                1hrs
    Store the game data to the server 2hrs
    Remote pong multiplayer           5hrs
    Write tests                       2hrs
    Make pretty.                      3hrs
    
    16hours left in about 30 hours of time. It shouldn't take me like half the
    time to do most of those things, but I want to stay safe with my time. I
    think I might just try to stick with local MP, just for the sake of time.

--- mp_pong/static/js/pong.js
+++ mp_pong/static/js/pong.js
@@ -21,8 +21,60 @@ function Game(){
         new_x = offset_x + current_x;
         new_y = offset_y + current_y;
 
-        setBallCoords(ball, new_x, new_y);
+        if (validateCoord(new_x, 'x') != true) {
+            //someone scored
+            alert('scored');
+            if (coord < 0){
+                scorePoint(player1, 1);}
+            else {
+                scorePoint(player2, 1);
+            }
+        }
+        else if (validateCoord(new_y, 'y') != true){
+            //ball hit wall, reverse x direction
+            console.log('hit wall');
+            offset_y *= -2;
+            reverse_y = true;
+            new_y = current_y + offset_y;
+
+            setBallCoords(ball, new_x, new_y);
+            return false;
+        }
+
+        else {
+            setBallCoords(ball, new_x, new_y);
+        }
     };
+
+    function scorePoint(plr, amount){
+        alert('score!');
+    }
+
+    function validateCoord(coord, x_or_y){
+
+        if (x_or_y == 'x'){
+            // alert('x testing');
+            console.log(coord);
+            console.log(limit_x);
+            if (coord < 0 || coord > limit_x){
+                alert('x false');
+                return false;}
+                // else {
+                //     return true;}
+        }
+        else if (x_or_y == 'y'){
+            console.log(coord);
+            console.log(limit_y);
+            if (coord < 0 || coord > limit_y){
+                console.log('y false');
+                return false;}
+                // else {
+                //     return true;}
+        }
+
+        return true;
+    }
+
 
     function setBallCoords(ball, x, y){
 
@@ -45,9 +97,19 @@ function Game(){
     function Update(){
 
         movePlayer(player1, 5);
-        moveBall(ball, 5, 5);
 
-        setTimeout(function() { Update()}, 50);
+
+        moveBall(ball, x_offset, y_offset);
+        if (reverse_y == true){
+            y_offset *= -1;
+            reverse_y = false;
+        }
+        if (reverse_x == true){
+            x_offset *=-1;
+            reverse_x = false;
+        }
+
+        setTimeout(function() { Update()}, 16);
     };
 
     function Start(){
@@ -56,6 +118,14 @@ function Game(){
         player2 = $($('.player2')[0]);
 
         ball= $($('.ball')[0]);
+        x_offset = 1;
+        y_offset = 5;
+
+        limit_x = parseInt($($('.play_area')[0]).css('width'));
+        limit_y = parseInt($($('.play_area')[0]).css('height'));
+
+        reverse_x = false;
+        reverse_y = false;
 
         movePlayer(player1, 5);
 
