57b0999ca736024371ac59d97fae344080b913e9
TankorSmash <tankorsmash@gmail.com>
2013-03-30 03:59:10 -0400

    Finally added the leaderboards.html, hasn't changed much but it needs to be here
    Have a matches played, won and the last 20 matches played. I haven't
    tested the last 20  stuff at all, but it is pretty straight forward so I
    hope I wrote it properly.
    
    I think because it's getting (ha!) late I'm not able to  think properly,
    meaning I'm starting to think I stand a chance. Going to play around with
    some Python stuff, see what I can muster in about an hour or so, including
    PEP8'ing the heck out of it, and then working on exporting it for
    FluidWare dudes to use.

--- josh_bergasse_pong/settings.py
+++ josh_bergasse_pong/settings.py
@@ -1,7 +1,8 @@
 # Django settings for josh_bergasse_pong project.
 
 DEBUG = True
-TEMPLATE_DEBUG = DEBUG
+#TEMPLATE_DEBUG = DEBUG
+TEMPLATE_DEBUG = True
 
 ADMINS = (
     # ('Your Name', 'your_email@example.com'),
